7fbff7c849859f9b48ee9a0695e06bdc9fd7e352
andel7 <lev@Levs-MacBook-Pro-3.local>
2014-08-18 21:06:57 +0300

    after refactor to functions

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
-	var end_number=prompt("Please enter a number");
-	console.log("entered "+end_number);
-
-	for (var i = 1; i <= end_number; i++) {
-		if ((i%5==0)&&(i%3==0)){
+	var end_number;
+	function do_the_thing(value){
+		if ((value%5==0)&&(value%3==0)){
 			console.log('fizzBuzz');
 		}
-		else if (i%5==0){
+		else if (value%5==0){
 			console.log('buzz');
-		} else if (i%3==0) {
+		} else if (value%3==0) {
 			console.log('fizz');
 		} else {
-			console.log(i);
+			console.log(value);
 		}
+		
+	}
+	function get_a_number(){
+		end_number=prompt("Please enter a number");
+		while(end_number % 1 != 0) {
+			alert("This is not a decimal number enter another one");
+			end_number=prompt("Please enter a number");
 
+		}
+		console.log("entered "+end_number);
+	}
+
+	get_a_number();
+
+	for (var i = 1; i <= end_number; i++) {
+		do_the_thing(i);
 	};
 })
